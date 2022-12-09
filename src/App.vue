68b4efb5c3e9998aa9184ab24bfd1ebb65127612
yy <691335336@qq.com>
2022-12-09 15:20:36 +0800

    [ADD] first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<!--
+ * @Author: yy 691335336@qq.com
+ * @Date: 2022-12-09 11:38:18
+ * @LastEditors: yy 691335336@qq.com
+ * @LastEditTime: 2022-12-09 15:15:18
+ * @FilePath: /go-view-yy/src/App.vue
+ * @Description: 
+-->
+<script setup lang="ts"></script>
+
+<template>
+	<router-view />
+</template>
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: yy 691335336@qq.com
+ * @Date: 2022-12-09 11:38:18
+ * @LastEditors: yy 691335336@qq.com
+ * @LastEditTime: 2022-12-09 15:13:09
+ * @FilePath: /go-view-yy/src/main.ts
+ * @Description: 
+ */
+import { createApp } from 'vue'
+import './style.css'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
